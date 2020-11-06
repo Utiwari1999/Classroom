9a91eaa795c8c4134915e23e75bcc03cabafedf7
jainil-4801 <201851116@iiitvadodara.ac.in>
2020-11-06 09:01:21 +0530

    Last update of 5/11/20

--- plag/process_file.py
+++ plag/process_file.py
@@ -1,5 +1,7 @@
 from docx import Document
 from .code.main import process
+from .text_matcher.matcher import Matcher,Text
+
 
 def process_file(filename):
     if filename.name.endswith('.docx'):
@@ -16,5 +18,36 @@ def process_file(filename):
     return process(data)
     
      
+def compare_two(filename1,filename2):
+    if filename1.name.endswith('.docx'):
+        document = Document(filename1)
+        fullText = []
+        data = ''
+        for para in document.paragraphs:
+            fullText.append(para.text)
+            data = '\n'.join(fullText)
+    else:
+        data = ''
+        for chunk in filename1.chunks():
+            data += chunk.decode('utf')
     
+    if filename2.name.endswith('.docx'):
+        document = Document(filename2)
+        fullText = []
+        data2 = ''
+        for para in document.paragraphs:
+            fullText.append(para.text)
+            data = '\n'.join(fullText)
+    else:
+        data2 = ''
+        for chunk in filename2.chunks():
+            data2 += chunk.decode('utf')
+    
+    ta = Text(data, 'A')
+    tb = Text(data2, 'B')
+
+    return Matcher(ta,tb).match()
+    
+     
+
     
--- plag/views.py
+++ plag/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import UploadFileForm
 from django.http import HttpResponseRedirect
-from .process_file import process_file
+from .process_file import process_file,compare_two
 from django.core.files.storage import FileSystemStorage
 import operator
 # Imaginary function to handle an uploaded file.
@@ -18,14 +18,13 @@ def upload_file(request):
         else:
             name = name + '.docx'
         filename = fs.save(name, myfile)
-        uploaded_file_url = fs.url(filename)
         c,output = process_file(myfile)
         render_obj = []
         for ele in sorted(output.items(),key=operator.itemgetter(1),reverse=True):
             if c[ele[0]]*100 > 20 and ele[1]>1:
-                render_obj.append([ele[1],ele[0],c[ele[0]]*100.00])
+                render_obj.append([ele[1],ele[0],round(c[ele[0]]*100.00,2)])
                 
-        return render(request, 'success.html', {
+        return render(request, 'output.html', {
             'render_obj': render_obj,
             'filename' : name,
         })
@@ -33,29 +32,31 @@ def upload_file(request):
 
 
 def compare_files(request):
-    if request.method == 'POST' and request.FILES['myfile1'] and request.FILES['myfiles2']:
+    if request.method == 'POST' and request.FILES['myfile1'] and request.FILES['myfile2']:
         myfile1 = request.FILES['myfile1']
         myfile2 = request.FILES['myfile2']
         label1 = request.POST.get('label1')
         label2 = request.POST.get('label2')
-        # fs = FileSystemStorage()
-        # if myfile.name.endswith('.txt'):
-        #     name = name + '.txt'
-        # else:
-        #     name = name + '.docx'
-        # filename = fs.save(name, myfile)
-        # uploaded_file_url = fs.url(filename)
-        # c,output = process_file(myfile)
-        # render_obj = []
-        # for ele in sorted(output.items(),key=operator.itemgetter(1),reverse=True):
-        #     if c[ele[0]]*100 > 20 and ele[1]>1:
-        #         render_obj.append([ele[1],ele[0],c[ele[0]]*100.00])
-                
+        
+
+        fs = FileSystemStorage()
+        if myfile1.name.endswith('.txt'):
+            label1 = label1 + '.txt'
+        else:
+            label1 = label1 + '.docx'
+        filename1 = fs.save(label1, myfile1)
+        
+        fs = FileSystemStorage()
+        if myfile2.name.endswith('.txt'):
+            label2 = label2 + '.txt'
+        else:
+            label2 = label2 + '.docx'
+        filename2 = fs.save(label2, myfile2)
+
+        output = compare_two(myfile1,myfile2)
+        
         # return render(request, 'success.html', {
         #     'render_obj': render_obj,
         #     'filename' : name,
         # })
-
-
-        print(myfile1.name,myfile2.name)
     return render(request, 'file_compare.html',{'message':"Hello World"})
